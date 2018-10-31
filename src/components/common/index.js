96a542d99ec02e7c39a70e8fc9025dc20624256e
procreatorx <44638829+procreatorx@users.noreply.github.com>
2018-10-31 16:39:22 +0530

    Add files via upload

---
+++ src/components/common/index.js
@@ -0,0 +1,7 @@
+export * from './Button';
+export * from './Header';
+export * from './CardSections';
+export * from './Card';
+export * from './input';
+export * from './Spinner';
+
---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux';
+import LibraryReducers from './LibraryReducer';
+import SelectionReducer from './SelectionReducer';
+
+export default combineReducers({
+
+    libraries: LibraryReducers,
+    selectedLibraryId: SelectionReducer
+
+});
